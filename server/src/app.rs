40f56479ccaf5c606397148fbe81b42b9485e12b
apetbrz <apetbrz@gmail.com>
2024-10-22 11:27:16 -0400

    removed old file

--- server/src/app.rs
+++
@@ -1,5 +0,0 @@
-//handler:  thread code to handle logged-in users
-//
-//TODO: be created on login, delete self on logout.
-//TODO: store JWT, server should route requests with the token to the appropriate thread
-//TODO: handle application data
